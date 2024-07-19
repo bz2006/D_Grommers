49ced5ea3692ee26fc1f6897ad67d353dd0ece82
bz2006 <bezelelb260@gmail.com>
2024-07-19 17:13:01 +0530

    slot ui

--- d_grommers/src/app/manage/slots/page.tsx
+++ d_grommers/src/app/manage/slots/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React, { useEffect, useState } from 'react'
 import axios from "axios"
-import { Button, Select, Collapse } from "antd"
+import { Button, Select, Collapse, Modal } from "antd"
 import type { CollapseProps } from 'antd';
 
 type Props = {}
@@ -38,6 +38,24 @@ const Slots = (props: Props) => {
 
   const [locations, setLocations] = useState<Loc[]>([]);
   const [selectedLocation, setSelectedLocation] = useState<GroomingSlot | null>(null);
+  const [selectedSlot, setselectedSlot] = useState<Slot | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = (slot: any) => {
+    setIsModalOpen(true);
+    console.log(slot);
+
+    setselectedSlot(slot);
+
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
 
 
   const fetchLocations = async () => {
@@ -70,12 +88,11 @@ const Slots = (props: Props) => {
     key: `${index}`,
     label: `${monthlySlot.month} ${monthlySlot.year}`,
     children: (
-      <div key={index}>
-        <h1 className='text-2xl pb-5'>Location: {selectedLocation?.district}</h1>
+      <div key={index} >
         <h2 className='text-xl pb-3'>{monthlySlot.month} {monthlySlot.year}</h2>
-        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-6 gap-x-1 gap-y-1'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-6 gap-x-1 gap-y-5'>
           {monthlySlot.slots?.map((slot, slotIndex) => (
-            <div key={slotIndex} className='flex flex-col p-5 bg-black w-40 items-center justify-center rounded-md text-white'>
+            <div key={slotIndex} onClick={() => { showModal(slot) }} className='flex flex-col p-5 bg-black w-40 items-center justify-center rounded-md text-white hover:cursor-pointer'>
               <h1>{slot.dayname}, {slot.day}</h1>
               <h1>{slot.avsl} Slots</h1>
               {/* <div>
@@ -126,11 +143,65 @@ const Slots = (props: Props) => {
 
       </div>
 
+      <div>
+        <h1 className={`text-2xl pb-5 p-10 text-white ${selectedLocation ? "block" : "hidden"}`}>Location: {selectedLocation?.district}</h1>
+      </div>
 
       <div className='p-10'>
         <Collapse items={items} />
-        
+
       </div>
+
+      <Modal title={`${selectedSlot?.dayname}, ${selectedSlot?.day}`} open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+
+        <div>
+
+          <div className='p-5 text-2xl pb-0'>
+            Slot Availability
+            <div className='pt-2'>
+              <hr className='w-96' />
+            </div>
+          </div>
+
+
+          <div className='p-5'>
+            <button className="bg-violet-500 text-white font-bold py-2 px-4 mr-5 rounded">
+              Available
+            </button>
+
+            <button className="bg-white-500 border border-black text-black  font-bold py-2 px-4 rounded">
+              Not Available
+            </button>
+
+          </div>
+
+        </div>
+
+
+
+        <div>
+          <div className='p-5 text-2xl pb-0'>
+            Time Slots
+            <div className='pt-2'>
+              <hr className='w-96' />
+            </div>
+          </div>
+
+          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-x-5 gap-y-5 p-5'>
+            {selectedSlot?.time.map((t, index) => (
+              <div
+                className='p-2 items-center justify-center flex min-w-10 border rounded-md border-black hover:cursor-pointer'
+                key={index}>
+                {t.time}
+              </div>
+            ))}
+            <div
+                className='p-2 items-center justify-center flex min-w-10 border border-dashed rounded-md border-black hover:cursor-pointer'>
+                +
+              </div>
+          </div>
+        </div>
+      </Modal>
 
     </div>
   )
